5ceed7428c4bd5b01061e7090eb90f18e99cd9e7
Sanjith0524 <sanjith.m2023@vitstudent.ac.in>
2024-06-06 22:56:34 +0530

    Deleting item from cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -13,7 +13,7 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingproduct.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -98,6 +98,8 @@ document.querySelectorAll('.js-delete-link').forEach((link) =>{
     link.addEventListener('click', () => {
     const productId =  link.dataset.productId;
     removeFromCart(productId);
-    console.log(cart);
+    
+    const item = document.querySelector(`.js-cart-item-container-${productId}`);
+    item.remove();
     })
 });
