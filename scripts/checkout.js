ed98fb46990f45b4010a98972ebea19056b93f76
Sanjith0524 <sanjith.m2023@vitstudent.ac.in>
2024-06-28 22:18:53 +0530

    Practiced promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,15 +2,24 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js';
 // import '../data/backend-practice.js'; 
 
-new Promise((resolve) => {
-    loadProducts(() => {
-        resolve();
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve('value1');
+        });
+    }),
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve('value1');
+        });
     })
-}).then(() => {
+]).then((values)=>{
     renderOrderSummary();
     renderCheckoutHeader();
     renderPaymentSummary();
 });
+
